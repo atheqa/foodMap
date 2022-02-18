23189c36aa903813a57dc99061c086127e6a3056
Heidi Kokkinen <heidi.kokkinen@gmail.com>
2022-02-19 00:00:52 +0200

    added functions to the select components

--- src/FoodPlan.js
+++ src/FoodPlan.js
@@ -1,17 +1,64 @@
 import React, { useState } from 'react';
-import Weekday from "./WeekDays"
+import Weekday from './WeekDays';
+
+const weekdays = [
+  { day: 'Monday', lunch: '', dinner: '' },
+  { day: 'Tuesday', lunch: '', dinner: '' },
+  { day: 'Wednesday', lunch: '', dinner: '' },
+  { day: 'Thursday', lunch: '', dinner: '' },
+  { day: 'Friday', lunch: '', dinner: '' },
+  { day: 'Saturday', lunch: '', dinner: '' },
+  { day: 'Sunday', lunch: '', dinner: '' },
+];
 
 const Foodplan = () => {
-    const [food, setFood] = useState("");
-    const weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+  const [data, setData] = useState(weekdays);
 
-    console.log(food)
+  function setLunch(day, ev) {
+    console.log(day, ev.target.value);
 
-    return weekdays.map(day => {
-        return <Weekday day={day} food={food} key={day} setFood={setFood} />
+    const list = data.map((item) => {
+      if (day === item.day) {
+        item.lunch = ev.target.value;
+        return item;
+      }
+
+      return item;
     });
+
+    console.log(list);
+
+    setData(list);
+  }
+
+  function setDinner(day, ev) {
+    console.log(day, ev.target.value);
+
+    const list = data.map((item) => {
+      if (day === item.day) {
+        item.dinner = ev.target.value;
+        return item;
+      }
+
+      return item;
+    });
+
+    console.log(list);
+    setData(list);
+  }
+
+  return data.map((item) => {
+    return (
+      <Weekday
+        day={item.day}
+        lunch={item.lunch}
+        dinner={item.dinner}
+        key={item.day}
+        setLunch={setLunch}
+        setDinner={setDinner}
+      />
+    );
+  });
 };
-
-
 
 export default Foodplan;
--- src/WeekDays.js
+++ src/WeekDays.js
@@ -5,46 +5,86 @@ import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
 
 const Box = styled.div`
-    width: 300px;
+  width: 320px;
+`;
+
+const Container = styled.div`
+  width: 300px;
+`;
+
+const ButtonContainer = styled.div`
+  width: 300px;
+  justify-content: center;
+  padding-top: 5px;
 `;
 
 const WeekDay = (props) => {
+  console.log(props.day);
 
-    const handleChange = (ev) => {
-        props.setFood(ev.target.value);
-      };
-
-      console.log(props.day)
-
-    return (
-        <Box>
-            Day: {props.day}
-            <FormControl fullWidth>
-                <InputLabel id="demo-simple-select-label">Food</InputLabel>
-                <Select
-                    labelId="demo-simple-select-label"
-                    id="demo-simple-select"
-                    value={props.food}
-                    label="Food"
-                    onChange={handleChange}
-                >
-                    <MenuItem value="quick-chicken-tomato-pasta">Quick Chicken-Tomato Pasta</MenuItem>
-                    <MenuItem value="fried-rice-with-chicken">Fried Rice with Chicken</MenuItem>
-                    <MenuItem value="tomato-tuna-soup">Tomato-Tuna Soup</MenuItem>
-                    <MenuItem value="lasagne">Lasagne</MenuItem>
-                </Select>
-            </FormControl>
-        </Box>
-    )
-
-}
+  return (
+    <Box>
+      <Container>
+        <p>Day: {props.day}</p>
+        <p>Lunch</p>
+        <FormControl fullWidth>
+          <InputLabel id="demo-simple-select-label">Food</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={props.lunch}
+            label="Food"
+            onChange={(ev) => props.setLunch(props.day, ev)}
+          >
+            <MenuItem value="quick-chicken-tomato-pasta">
+              Quick Chicken-Tomato Pasta
+            </MenuItem>
+            <MenuItem value="fried-rice-with-chicken">
+              Fried Rice with Chicken
+            </MenuItem>
+            <MenuItem value="tomato-tuna-soup">Tomato-Tuna Soup</MenuItem>
+            <MenuItem value="lasagne">Lasagne</MenuItem>
+          </Select>
+        </FormControl>{' '}
+        <p>Dinner</p>
+        <FormControl fullWidth>
+          <InputLabel id="demo-simple-select-label">Food</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={props.dinner}
+            label="Food"
+            onChange={(ev) => props.setDinner(props.day, ev)}
+          >
+            <MenuItem value="quick-chicken-tomato-pasta">
+              Quick Chicken-Tomato Pasta
+            </MenuItem>
+            <MenuItem value="fried-rice-with-chicken">
+              Fried Rice with Chicken
+            </MenuItem>
+            <MenuItem value="tomato-tuna-soup">Tomato-Tuna Soup</MenuItem>
+            <MenuItem value="lasagne">Lasagne</MenuItem>
+          </Select>
+        </FormControl>{' '}
+      </Container>
+      <ButtonContainer>
+        <Stack direction="row" spacing={2} justifyContent="center">
+          <Button variant="outlined">Save</Button>
+        </Stack>
+      </ButtonContainer>
+    </Box>
+  );
+};
 
 WeekDay.propTypes = {
-    day: PropTypes.string,
-    food: PropTypes.string,
-    setFood: PropTypes.func,
+  day: PropTypes.string,
+  dinner: PropTypes.string,
+  lunch: PropTypes.string,
+  setLunch: PropTypes.func,
+  setDinner: PropTypes.func,
 };
 
 export default WeekDay;
